c464794621ecf2e09d9e782beb7d2ec46404b518
nibnil <nibnilnehc@gmail.com>
2021-01-27 00:08:12 +0800

    Initial commit

---
+++ data-helper.py
@@ -0,0 +1,154 @@
+import logging
+from time import sleep
+
+import requests
+from lxml import etree
+
+from mongodb_helper import MongodbHelper
+
+file_handler = logging.FileHandler('sanguo-data.log')
+stream_handler = logging.StreamHandler()
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)-s - %(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[file_handler, stream_handler])
+
+GW_URL = 'http://so.17173.com/monster/gw{0}.shtml'
+BOSS_URL = 'http://so.17173.com/monster/boss{0}.shtml'
+
+
+def get_data(url, html_type=0):
+    logging.info(f'GetData, url: {url}')
+    data = list()
+    resp = requests.get(url)
+    text = resp.text
+    html = etree.HTML(text)
+    if html_type == 0:
+        elements = html.xpath(
+            '//html/body/table[2]/tr[2]/td[2]/table/tr[2]/td/table/tr[1]/td/table/tr[3]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tr')
+    else:
+        elements = html.xpath(
+            '//html/body/table[2]/tr[2]/td[2]/table/tr[2]/td/table/tr[1]/td/table/tr[3]/td/table/tbody/tr[2]/td/table/tr')
+    first = True
+    for element in elements:
+        if first is True:
+            name = element.xpath('td/table/tbody/tr[2]/td[1]/text()')[0]
+            level = element.xpath('td/table/tbody/tr[2]/td[2]/text()')[0]
+            try:
+                level = int(level)
+            except Exception as ex:
+                pass
+            maps = element.xpath('td/table/tbody/tr[2]/td[6]/text()')[0]
+            maps = maps.split('，')
+            products = element.xpath('td/table/tbody/tr[2]/td[7]/text()')[0]
+            products = products.split(',')
+            for product in products:
+                if product == '':
+                    products.remove(product)
+            first = False
+        else:
+            name = element.xpath('td/table/tbody/tr[3]/td[1]/text()')[0]
+            level = element.xpath('td/table/tbody/tr[3]/td[2]/text()')[0]
+            try:
+                level = int(level)
+            except Exception as ex:
+                pass
+            maps = element.xpath('td/table/tbody/tr[3]/td[6]/text()')[0]
+            maps = maps.split('，')
+            products = element.xpath('td/table/tbody/tr[3]/td[7]/text()')[0]
+            products = products.split(',')
+            for product in products:
+                if product == '':
+                    products.remove(product)
+        data.append({'name': name,
+                     'level': level,
+                     'maps': maps,
+                     'products': products})
+    return data
+
+
+def get_boss_data(url, html_type=0):
+    logging.info(f'GetData, url: {url}')
+    data = list()
+    resp = requests.get(url)
+    text = resp.text
+    html = etree.HTML(text)
+    if html_type == 0:
+        elements = html.xpath(
+            '//html/body/table[2]/tr[2]/td[2]/table/tr[2]/td/table/tr[1]/td/table/tr[3]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tr')
+    else:
+        elements = html.xpath(
+            '//html/body/table[2]/tr[2]/td[2]/table/tr[2]/td/table/tr[1]/td/table/tr[3]/td/table/tbody/tr[2]/td/table/tr')
+    first = True
+    for element in elements:
+        if first is True:
+            name = element.xpath('td/table/tbody/tr[2]/td[1]/text()')[0]
+            level = element.xpath('td/table/tbody/tr[2]/td[2]/text()')[0]
+            try:
+                level = int(level)
+            except Exception as ex:
+                pass
+            maps = element.xpath('td/table/tbody/tr[2]/td[6]/text()')[0]
+            maps = maps.split('，')
+            products = element.xpath('td/table/tbody/tr[2]/td[7]/text()')[0]
+            products = products.split(',')
+            for product in products:
+                if product == '':
+                    products.remove(product)
+            first = False
+        else:
+            name = element.xpath('td/table/tbody/tr[3]/td[1]/text()')[0]
+            level = element.xpath('td/table/tbody/tr[3]/td[2]/text()')[0]
+            try:
+                level = int(level)
+            except Exception as ex:
+                pass
+            maps = element.xpath('td/table/tbody/tr[3]/td[6]/text()')[0]
+            maps = maps.split('，')
+            products = element.xpath('td/table/tbody/tr[3]/td[7]/text()')[0]
+            products = products.split(',')
+            for product in products:
+                if product == '':
+                    products.remove(product)
+        print({'name': name,
+                     'level': level,
+                     'maps': maps,
+                     'products': products})
+        data.append({'name': name,
+                     'level': level,
+                     'maps': maps,
+                     'products': products})
+    return data
+
+
+def main():
+    db = MongodbHelper()
+    # for i in range(1, 62):
+    #     index = "{:0>2d}".format(i)
+    #     url = GW_URL.format(index)
+    #     if i == 1:
+    #         data = get_boss_data(url, html_type=0)
+    #     else:
+    #         data = get_boss_data(url, html_type=1)
+    #     result = db.do_bulk_upsert(col='monster',
+    #                                data=data,
+    #                                filter_key=['name', 'level', 'maps', 'products'],
+    #                                set_on_insert_key=['name', 'level', 'maps', 'products'])
+    #     print(result)
+    for i in range(1, 56):
+        index = "{:0>2d}".format(i)
+        url = BOSS_URL.format(index)
+        if i == 1:
+            data = get_boss_data(url, html_type=0)
+        else:
+            data = get_boss_data(url, html_type=1)
+        result = db.do_bulk_upsert(col='monster',
+                                    data=data,
+                                    filter_key=['name', 'level', 'maps', 'products'],
+                                    set_on_insert_key=['name', 'level', 'maps', 'products'])
+        print(result)
+        sleep(5)
+
+
+if __name__ == '__main__':
+    main()
---
+++ mongodb_helper.py
@@ -0,0 +1,61 @@
+import logging
+
+from pymongo import MongoClient, UpdateOne
+
+
+class MongodbHelper:
+    def __init__(self, host='192.168.31.22', port=27017, db='sanguo'):
+        self._host = host
+        self._port = port
+        self._client = MongoClient(host=host, port=port)
+        self._db = self._client.get_database(db)
+
+    def __del__(self):
+        pass
+
+    def do_bulk_upsert(self,
+                       col: str,
+                       data: list,
+                       filter_key: list,
+                       set_key: list = None,
+                       set_on_insert_key: list = None):
+        try:
+            collection = self._db[col]
+            requests = list()
+            for d in data:
+                filter_ = dict()
+                for key in filter_key:
+                    filter_.update({key: d.get(key)})
+                set_on_insert = dict()
+                set_ = dict()
+                if set_on_insert_key:
+                    for key in set_on_insert_key:
+                        value = d.pop(key)
+                        set_on_insert[key] = value
+                if set_key:
+                    for key in set_key:
+                        value = d.pop(key)
+                        set_[key] = value
+                else:
+                    if d:
+                        set_ = d
+                        update = {'$set': set_,
+                                  '$setOnInsert': set_on_insert}
+                    else:
+                        update = {'$setOnInsert': set_on_insert}
+                request = UpdateOne(filter=filter_, update=update, upsert=True)
+                requests.append(request)
+            if requests:
+                result = collection.bulk_write(requests=requests, ordered=True)
+                logging.info(f'DoBulkWrite, '
+                             f'col({col}), total({len(data)}), '
+                             f'modified({result.modified_count}), upserted({result.upserted_count})')
+                return result
+            else:
+                logging.info(f'DoBulkWrite, requests is empty, filter key({filter_key})')
+                return None
+        except Exception as ex:
+            logging.exception(ex)
+            return None
+
+
